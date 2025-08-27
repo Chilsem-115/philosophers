ae0900c9c3ec581d61a52d0fec819399d6438ed2
Ibrahim Tamsamani <itamsama@student.1337.ma>
2025-08-27 08:01:04 +0100

    ez

--- philo/main/app.c
+++ philo/main/app.c
@@ -6,11 +6,15 @@
 /*   By: itamsama <itamsama@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/27 06:36:48 by itamsama          #+#    #+#             */
-/*   Updated: 2025/08/27 06:55:43 by itamsama         ###   ########.fr       */
+/*   Updated: 2025/08/27 08:00:23 by itamsama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "app.h"
+
+static void	set_philo(t_sim *sim)
+{
+}
 
 int	main(int argc, char **argv)
 {
@@ -18,4 +22,5 @@ int	main(int argc, char **argv)
 
 	if (parse(&sim, argc, argv))
 		return (0);
+	set_philo(&sim);
 }
--- philo/main/app.h
+++ philo/main/app.h
@@ -6,7 +6,7 @@
 /*   By: itamsama <itamsama@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/27 06:37:11 by itamsama          #+#    #+#             */
-/*   Updated: 2025/08/27 06:56:01 by itamsama         ###   ########.fr       */
+/*   Updated: 2025/08/27 07:43:13 by itamsama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,21 +24,23 @@
 
 typedef struct s_philo
 {
-	int			id;
+	int				id;
+	pthread_t		thread;
 	pthread_mutex_t	left;
 	pthread_mutex_t	right;
-	long		last_meal_ms;
-	int			meals;
-	pthread_t	thread;
+	long			last_meal_ms;
+	int				meals;
+	int				is_dead;
 }	t_philo;
 
 typedef struct s_sim
 {
-	int	number_of_philosophers;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	max_eat_count;
+	int		number_of_philosophers;
+	int		time_to_die;
+	int		time_to_eat;
+	int		time_to_sleep;
+	int		max_eat_count;
+	t_philo	*philo;
 }	t_sim;
 
 /* utils */
